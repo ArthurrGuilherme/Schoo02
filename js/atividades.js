1f4e9bac02210b5cb3592c95d7923b20639f7d0b
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-03-20 22:15:09 -0300

    Add files via upload

--- js/atividades.js
+++ js/atividades.js
@@ -1,3 +1,4 @@
+/*Questão 01 */
 document.querySelectorAll(".checkResposta").forEach(check => {
     check.addEventListener("change", function() {
         document.querySelectorAll(".checkResposta").forEach(chk => chk !== this && (chk.checked = false));
@@ -13,6 +14,27 @@ function verificarResposta() {
             swal("Parabéns!", "Você escolheu a resposta certa!", "success");
         } else {
             swal("Alternativa correta:", "d) Empresas de fast food adaptando seus cardápios para incluir opções  saudáveis. Você deve lembrar do exemplo da indústria de fast food ou dos restaurantes durante a pandemia. Para a adaptabilidade, destaca-se como as empresas ajustaram seus modelos de negócio para atender às mudanças na demanda (exemplo: introdução de  opções saudáveis). Para a resiliência, reconhecemos como as empresas superaram adversidades, reinventando suas operações para continuar  funcionando.", "error");
+        }
+    } else {
+        swal("Atenção!", "Selecione uma opção primeiro!", "warning");
+    }
+}
+/*Questão 02 */
+document.querySelectorAll(".checkResposta1").forEach(check => {
+    check.addEventListener("change", function() {
+        document.querySelectorAll(".checkResposta1").forEach(chk => chk !== this && (chk.checked = false));
+        document.getElementById("botaoResposta1").style.display = "block";
+    });
+});
+
+function verificarResposta1() {
+    let selecionado = document.querySelector(".checkResposta1:checked");
+    if (selecionado) {
+        let correto = selecionado.getAttribute("data-correto") === "true";
+        if (correto) {
+            swal("Parabéns!", "Você escolheu a resposta certa!", "success");
+        } else {
+            swal("Alternativa correta:", " a) Flexibilidade, autocontrole emocional, tolerância ao estresse e coragem Além de identificar os quatro pilares (pilares estresse e coragem), é importante que você conecte esses pilares a uma experiência pessoal, como adaptar-se a aulas online (flexibilidade)  gerenciar o estresse de uma avaliação difícil (autocontrole emocional) aprender com feedback negativo em um projeto (foco no aprendizado) ou buscar ajuda de colegas e professores (rede de suporte).", "error");
         }
     } else {
         swal("Atenção!", "Selecione uma opção primeiro!", "warning");
